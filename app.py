13f12f646c5d9d6c45d5a1c1ed3f5dd13db08e6f
alina <zemlyakova.alina@yandex.ru>
2022-12-21 21:45:37 +0300

    mysql api added

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 from analyzer import Analyzer
 
+from flask_mysqldb import MySQL
+
 app = Flask(__name__, template_folder='templates')
 
 analyzer = Analyzer('checkpoints/bert_model_0_1.pt')
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234567890'
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
 
 @app.route('/')
 def tryit():
@@ -20,6 +29,20 @@ def predict_label():
     pred_label = analyzer.predict_label(text)
     print(pred_label)
     return render_template('tryit.html', context={'label': pred_label})
+
+cur = mysql.connection.cursor()
+
+def insert_into_db():
+  text = request.form['text']
+  cur.execute(''' INSERT INTO nlg_label ('text') VALUES(%s)''',(text))
+  mysql.connection.commit()
+
+def insert_label():
+    if "correct" in request.form:
+        cur.execute(''' INSERT INTO nlg_label ('label') VALUES('correct')''')
+    elif "incorrect" in request.form:
+        cur.execute(''' INSERT INTO nlg_label ('label') VALUES('correct')''')
+    return render_template('tryit.html')
 
 if __name__ == '__main__':
     app.run()
